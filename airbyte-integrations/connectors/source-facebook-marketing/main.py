aeeab5c687aa1395cc00775bdaf8b69860d419be
Anatolii Yatsuk <tolikyatsuk@gmail.com>
2023-12-20 21:58:50 +0200

    Add migration to new property account_ids

--- airbyte-integrations/connectors/source-facebook-marketing/main.py
+++ airbyte-integrations/connectors/source-facebook-marketing/main.py
@@ -7,7 +7,9 @@ import sys
 
 from airbyte_cdk.entrypoint import launch
 from source_facebook_marketing import SourceFacebookMarketing
+from source_facebook_marketing.config_migrations import MigrateAccountIdToArray
 
 if __name__ == "__main__":
     source = SourceFacebookMarketing()
+    MigrateAccountIdToArray.migrate(sys.argv[1:], source)
     launch(source, sys.argv[1:])
